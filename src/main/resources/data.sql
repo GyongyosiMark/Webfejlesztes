dc4eb0a4cc7c13fed3d462508432c6b58985fdca
Kaiusz <55788794+Kaiusz@users.noreply.github.com>
2021-12-04 13:33:09 +0100

    upload

---
+++ src/main/resources/data.sql
@@ -0,0 +1,15 @@
+insert into DEPARTMENT_MASTER values(1001,'Sci-fi');
+insert into DEPARTMENT_MASTER values(1002,'Fantasy');
+insert into DEPARTMENT_MASTER values(1003,'Non-fiction');
+insert into DEPARTMENT_MASTER values(1004,'History');
+insert into DEPARTMENT_MASTER values(1005,'Crime');
+insert into DEPARTMENT_MASTER values(1006,'Mystery');
+insert into DEPARTMENT_MASTER values(1007,'Horror');
+
+
+
+insert into EMP_INFO(emp_Id, EMP_NAME, PASSWORD, ADDRESS, EMAIL_ID, DEPT_ID ) values(101,'Dune','Frank Herbert','1965','english', 1001);
+insert into EMP_INFO(emp_Id, EMP_NAME, PASSWORD, ADDRESS, EMAIL_ID, DEPT_ID ) values(102,'It','Stephen King','1986','english', 1007);
+insert into EMP_INFO(emp_Id, EMP_NAME, PASSWORD, ADDRESS, EMAIL_ID, DEPT_ID ) values(103,'Sapiens: A Brief History of Humankind','Yuval Noah Harari','2011','hebrew', 1003);
+insert into EMP_INFO(emp_Id, EMP_NAME, PASSWORD, ADDRESS, EMAIL_ID, DEPT_ID ) values(104,'The Last Wish','Andrzej Sapkowski','1993','polish', 1002);
+insert into EMP_INFO(emp_Id, EMP_NAME, PASSWORD, ADDRESS, EMAIL_ID, DEPT_ID ) values(105,'Murder on the Orient Express','Agatha Christie','1934','english', 1005);
